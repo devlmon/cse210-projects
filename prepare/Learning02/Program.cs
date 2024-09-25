dcbf4a468be1c8f2addcfc22e30699ebe0b79207
devlmon <luis.montenegro7@outlook.com>
2024-09-25 10:22:02 -0400

    Week 2 Prep assigment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //First job instance
+        Job job1        = new Job();
+        job1._company   = "Perfect Company";
+        job1._jobTitle  = "Programmer";
+        job1._startYear = "2001";
+        job1._endYear   = "2023";
+
+        //Second job instance
+        Job job2        = new Job();
+        job2._company   = "Super Awesome Company";
+        job2._jobTitle  = "Writter";
+        job2._startYear = "2001";
+        job2._endYear   = "2023";
+
+        //My resume instance
+        Resume myresume = new Resume();
+        myresume._name  = "Luis Montenegro";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        //Output
+        myresume.Display();
     }
 }
